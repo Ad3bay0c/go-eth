56bd455eee24dbecdf0ac75cd2171ad8819f5295
Ad3bay0c <aclintonadebayo@gmail.com>
2022-09-22 09:24:57 +0100

    implement get balance and get bloc number

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"github.com/Ad3bay0c/eth-contract-go/wallet"
 	"log"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 )
+
+const address = "0x5fD630A0470cd7E21624a494c309d40DFe82633D"
 
 func main() {
 	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("we have a connection now: %v", client)
+
+	balance, err := wallet.GetBalance(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(balance, "ETH")
+	log.Println(wallet.GetBlockNumber(client))
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,24 @@
 package wallet
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func GetBalance(key string, client *ethclient.Client) (*big.Int, error) {
+	account := common.HexToAddress(key)
+
+	return client.BalanceAt(context.Background(), account, nil)
+}
+
+func GetBlockNumber(client *ethclient.Client) (uint64, error) {
+
+	blockNumber, err := client.BlockNumber(context.Background())
+	if err != nil {
+		return 0, err
+	}
+	return blockNumber, nil
+}
